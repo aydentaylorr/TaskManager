bab4ee6ab40c778e1c1c3d4e014e620c9fcbe9b0
AydenS <AydenS@AYDEN>
2026-02-18 09:22:01 +0200

    Add project files.

---
+++ TaskManager.Models/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManager.Models.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
---
+++ TaskManager.WinForms/Forms/LoginForm.cs
@@ -0,0 +1,10 @@
+namespace TaskManager.WinForms
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
